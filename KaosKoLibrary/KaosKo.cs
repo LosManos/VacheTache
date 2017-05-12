2daff90b65b3ea25c461f006a61888909ea6e2ef
LosManos <gitkraken.20.olafj@spamgourmet.com>
2017-05-12 22:57:30 +0200

    Added Int, PositiveInt and Guid methods.
    First commit.

---
+++ KaosKoLibrary/KaosKo.cs
@@ -0,0 +1,108 @@
+﻿namespace KaosKoLIbrary
+{
+    using System;
+    using System.Runtime.CompilerServices;
+
+    public class KaosKo
+    {
+        private Random _rand;
+
+        /// <summary>This constructor behaves like he default constructor
+        /// and is used for seeding the random values with a known seed.
+        /// </summary>
+        /// <param name="seed">Optional. 
+        /// If left out the caller's method name is used for seeding the randomising.
+        /// If provided the parameters is hashed and then used for seeding the randomising.</param>
+        public KaosKo([CallerMemberName] string seed = null)
+            : this(HashCode, seed)
+        {
+        }
+
+        /// <summary>This constructor is used for using an own method for hashing the seed.
+        /// </summary>
+        /// <param name="hashCodeFunc"></param>
+        /// <param name="seed"></param>
+        public KaosKo(Func<string, int> hashCodeFunc, string seed)
+        {
+            _rand = new Random(hashCodeFunc(seed));
+        }
+
+        /// <summary>This method creates a new GUIDish value.
+        /// It is not a proper GUID as a such adheres to certain rules, for instance there are presently 4 different GUID versions and the generating of randomised values below does not take this into consideration.
+        /// The code is copied from http://stackoverflow.com/a/13188409/521554
+        /// </summary>
+        /// <returns></returns>
+        public Guid Guid()
+        {
+            var guid = new byte[16];
+            _rand.NextBytes(guid);
+            return new Guid(guid);
+        }
+
+        /// <summary>This method returns a randomised integer
+        /// same or above int.MinValue and
+        /// below int.MaxValue.
+        /// It is really just a pass through to Random.Next(int.MinValue, int.MaxValue);
+        /// </summary>
+        /// <returns></returns>
+        public int Int()
+        {
+            return _rand.Next(int.MinValue, int.MaxValue);
+        }
+
+        /// <summary>This method returns a randomised integer below maxValue and above or same as minValue.
+        /// Note lower-than-max and not lower-or-equal-to-max.
+        /// The reason for this is that the called Random.Next method behaves this way
+        /// and right or wrong we keep with it.
+        /// It is really just a pass through to Random.Next(int min, int max);
+        /// </summary>
+        /// <param name="minValue"></param>
+        /// <param name="maxValue"></param>
+        /// <returns></returns>
+        public int Int(int minValue, int maxValue)
+        {
+            return _rand.Next(minValue, maxValue);
+        }
+
+        /// <summary>This method returns a randomised integer between 0 and int.MaxValue.
+        /// It is really just a pass through to Random.Next();
+        /// </summary>
+        /// <returns></returns>
+        public int PositiveInt()
+        {
+            return _rand.Next();
+        }
+
+        /// <summary>This method returns a randomised integer below maxValue.
+        /// Note lower-than-max and not lower-or-equal-to-max.
+        /// The reason for this is that the called Random.Next method behaves this way
+        /// and right or wrong we keep with it.
+        /// It is really just a pass through to Random.Next(int max);
+        /// </summary>
+        /// <param name="maxValue"></param>
+        /// <returns></returns>
+        public int PositiveInt(int maxValue)
+        {
+            return _rand.Next(maxValue);
+        }
+
+        /// <summary>This is a (simple) hash algorithm.
+        /// We don't use the built in GetHashCode since it might
+        /// change value between every run.
+        /// <seealso cref="http://stackoverflow.com/q/41183507/521554"/>
+        /// <seealso cref="https://docs.microsoft.com/en-us/dotnet/api/system.string.gethashcode?view=netcore-1.1"/>
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        private static int HashCode(string s)
+        {
+            //return s.Select(a => (int)a).Sum();
+            var ret = 0;
+            foreach( var c in s ?? string.Empty)
+            {
+                ret += (int)c;
+            }
+            return ret;
+        }
+    }
+}
---
+++ Tests/KaosKoLibraryTest/KaosKoTest.cs
@@ -0,0 +1,196 @@
+namespace KaosKoLibraryTest
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using KaosKoLIbrary;
+    using System.Linq;
+
+    [TestClass]
+    public class KaosKoTest
+    {
+        #region Constructor tests.
+
+        [TestMethod]
+        public void KaosKo_DifferentSeed_ReturnDifferentResult()
+        {
+            //  #   Arrange.
+            var sut1 = new KaosKo("Seed1");
+            var sut2 = new KaosKo("Seed2");
+
+            //  #   Act.
+            var res1 = sut1.PositiveInt();
+            var res2 = sut2.PositiveInt();
+
+            //  #   Assert.
+            Assert.AreNotEqual(res1, res2);
+        }
+
+        [TestMethod]
+        public void KaosKo_NoSeed_UseCallerName()
+        {
+            //  #   Arrange.
+            var sutKnownSeed = new KaosKo(nameof(KaosKo_NoSeed_UseCallerName));
+            var sutMethodSeed = new KaosKo();
+
+            //  #   Act.
+            var knownSeedResult = sutKnownSeed.PositiveInt();
+            var methodSeedResult = sutMethodSeed.PositiveInt();
+
+            //  #   Assert.
+            Assert.AreEqual(knownSeedResult, methodSeedResult);
+        }
+
+        [TestMethod]
+        public void KaosKo_HashCodeFunc_NewMethod_UseIt()
+        {
+            //  #   Arrange.
+            // Create a hashing function that always returns the same hash code.
+           var hashingFunction = new Func<string, int>(s => 0);
+
+            //  #   Act.
+            var res1 = new KaosKo(hashingFunction, "a seed").PositiveInt();
+            var res2 = new KaosKo(hashingFunction, "another seed").PositiveInt();
+
+            //  #   Assert.
+            Assert.AreEqual(res1, res2);
+            Assert.AreEqual(1559595546, res1);
+        }
+
+        [TestMethod]
+        public void KaosKo_SameSeed_ReturnSameResult()
+        {
+            //  #   Arrange.
+            const string Seed = "MySeed";
+            var sut1 = new KaosKo(Seed);
+            var sut2 = new KaosKo(Seed);
+
+            //  #   Act.
+            var res1 = new[] { sut1.PositiveInt(), sut1.PositiveInt(), sut1.PositiveInt() };
+            var res2 = new[] { sut2.PositiveInt(), sut2.PositiveInt(), sut2.PositiveInt() };
+
+            //  #   Assert.
+            CollectionAssert.AreEqual(res1, res2);
+        }
+
+        #endregion
+
+        #region Guid tests.
+
+        [TestMethod]
+        public void Guid_ReturnValueGuid()
+        {
+            //  #   Arrange.
+            var sut = new KaosKo();
+
+            //  #   Act.
+            var res = sut.Guid();
+
+            //  #   Assert.
+            Assert.IsTrue(Guid.TryParse(res.ToString(), out _));
+        }
+
+        #endregion
+
+        #region Int tests.
+
+        [TestMethod]
+        public void Int_NoParameter_ReturnSameOrAboveIntMinAndBelowIntMax()
+        {
+            //  Without doing lots of calls there is not much we can test here.
+            //  So lets fall back to calling it to make sure we have tested the interface.
+
+            //  #   Arrange.
+            var sut = new KaosKo();
+
+            //  #   Act and Assert.
+            for (int i = 0; i < 1_000_000; i++)
+            {
+                var res = sut.Int();
+                Assert.IsTrue(int.MinValue <= res && res < int.MaxValue);
+            }
+        }
+
+        [TestMethod]
+        public void Int_MinAndMax_ReturnBetweenInclusiveMinAndExclusiveMax()
+        {
+            //  #   Arrange.
+            var sut = new KaosKo();
+            const int Min = -12;
+            const int Max = 420;
+            var results = new int[Max];
+
+            //  #   Act and Assert.
+            for (int i = 0; i < 1_000_000; i++)
+            {
+                var res = sut.PositiveInt(Max);
+                Assert.IsTrue(Min <= res && res < Max);
+
+                //  Keep a tab on how many times each integer value was returned.
+                results[res] += 1;
+            }
+
+            Assert.IsTrue(
+                results.All(r => r >= 1),
+                "If we are iterating that many times it would be strange if not all integers was returns at least once.");
+        }
+
+        #endregion
+
+        #region PositiveInt tests.
+
+        [TestMethod]
+        public void PoistiveInt_ReturnPredictableResult()
+        {
+            //  #   Arrange.
+            var sut = new KaosKo("MySeed");
+
+            //  #   Act.
+            var res = new[] { sut.PositiveInt(), sut.PositiveInt(), sut.PositiveInt() };
+
+            //  #   Assert.
+            CollectionAssert.AreEqual(
+                new[] { 644677688, 1890600396, 597045847 },
+                res,
+                $"Values were [{res[0]},{res[1]},{res[2]}].");
+        }
+
+        [TestMethod]
+        public void PositiveInt_ReturnPositive()
+        {
+            //  #   Arrange.
+            var sut = new KaosKo();
+
+            //  #   Act and assert.
+            for (int i = 0; i <= 1_000_000; i++)
+            {
+                var res = sut.PositiveInt();
+                Assert.IsTrue(0 <= res && res <= int.MaxValue);
+            }
+        }
+
+        [TestMethod]
+        public void PositiveInt_Max_ReturnBelow()
+        {
+            //  #   Arrange.
+            var sut = new KaosKo();
+            const int Max = 420;
+            var results = new int[Max];
+
+            //  #   Act and Assert.
+            for (int i = 0; i < 1_000_000; i++)
+            {
+                var res = sut.PositiveInt(Max);
+                Assert.IsTrue(0 <= res && res < Max);
+
+                //  Keep a tab on how many times each integer value was returned.
+                results[res] += 1; 
+            }
+
+            Assert.IsTrue(
+                results.All(r => r >= 1), 
+                "If we are iterating that many times it would be strange if not all integers was returns at least once.");
+        }
+
+        #endregion
+    }
+}
